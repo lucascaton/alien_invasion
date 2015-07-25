e21a0f4c3ec758393c578364d4e5b6eef810f9b9
Lucas Caton <lucascaton@gmail.com>
2015-07-26 08:27:15 +1000

    Refactoring Map class and adding begin and end paths

--- lib/alien_invasion/map.rb
+++ lib/alien_invasion/map.rb
@@ -1,16 +1,16 @@
 module AlienInvasion
   class Map
     TILE_SIZE = 85 # px
-    MAP_PATH, MAP_BEGIN, MAP_END = %w(p b e)
+    PATHS = { normal: '.', begin: '^', end: '$' }
+    IMAGES = PATHS.keys.map { |path| [path, Gosu::Image.new("images/paths/#{path}.png", tileable: true)] }.to_h
     Z_ORDER = AlienInvasion::GameConfig.z_order(:paths)
 
     def initialize(name)
-      @path_image  = Gosu::Image.new('images/path.png', tileable: true)
       @coordinates = generate_coordinates(name)
     end
 
     def draw
-      @coordinates.each { |x, y| @path_image.draw(x, y, Z_ORDER) }
+      @coordinates.each { |x, y, path| IMAGES[path].draw(x, y, Z_ORDER) }
     end
 
     private
@@ -25,16 +25,16 @@ module AlienInvasion
 
       map_file.split.map(&:chars).each_with_index do |row, row_index|
         row.each_with_index do |column, column_index|
-          if column == MAP_PATH
-            x = width_base  + (column_index * TILE_SIZE)
-            y = height_base + (row_index    * TILE_SIZE)
+          next unless PATHS.values.include?(column)
 
-            if ENV['DEVELOPMENT'] == 'true'
-              puts "#{row_index + 1} x #{column_index + 1}\t(#{x.round}px x #{y.round}px)"
-            end
+          x = width_base  + (column_index * TILE_SIZE)
+          y = height_base + (row_index    * TILE_SIZE)
 
-            coordinates << [x, y]
+          if ENV['DEVELOPMENT'] == 'true'
+            puts "#{row_index + 1} x #{column_index + 1}\t(#{x.round}px x #{y.round}px)"
           end
+
+          coordinates << [x, y, PATHS.key(column)]
         end
       end
 
