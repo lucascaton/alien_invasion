428a9e20ebfa9f3d5589ecceb4f36b36c1519649
Lucas Caton <lucascaton@gmail.com>
2015-07-26 11:22:07 +1000

    Creating Aliens::Ob::IMAGE constant

--- lib/alien_invasion/aliens/ob.rb
+++ lib/alien_invasion/aliens/ob.rb
@@ -2,13 +2,13 @@ module AlienInvasion
   module Aliens
     class Ob
       Z_ORDER = AlienInvasion::GameConfig.z_order(:aliens)
+      IMAGE = Gosu::Image.new('images/aliens/ob.png')
 
       def initialize(map:, speed:, hp:)
         @map   = map
         @speed = speed
         @hp    = hp
 
-        @image = Gosu::Image.new('images/aliens/ob.png')
         @x, @y = @map.get_coordinates(:begin)
       end
 
@@ -18,7 +18,7 @@ module AlienInvasion
       end
 
       def draw
-        @image.draw(@x, @y, Z_ORDER)
+        IMAGE.draw(@x, @y, Z_ORDER)
       end
     end
   end
